56777a3b98360268d9d1cd548218f65d96865c1c
idume victor <idumevic@gmail.com>
2023-04-20 08:56:39 +0100

    onboarding done

--- lib/onboarding_screens.dart
+++ lib/onboarding_screens.dart
@@ -16,103 +16,101 @@ class _OnboardingState extends State<Onboarding> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        
-          child: Padding(
-          
-            padding: EdgeInsets.only(left: 20, right: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height:54,
-                  child: Center(
-                    child: Text(
-                      'Welcome to Sendy.',
-                      style: TextStyle(
-                          color: Color(0xff484848),
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          fontFamily: 'Lato-Regular'),
-                    ),
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 54,
+                child: Center(
+                  child: Text(
+                    'Welcome to Sendy.',
+                    style: TextStyle(
+                        color: Color(0xff484848),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Lato-Regular'),
                   ),
                 ),
-                SizedBox(
-                  height: 93,
+              ),
+              SizedBox(
+                height: 93,
+              ),
+              Image(
+                image: AssetImage('images/onboarding.png'),
+              ),
+              SizedBox(
+                height: 93,
+              ),
+              Text(
+                'Manage your finance in\nthe easiest way.',
+                style: TextStyle(
+                  color: Color(0xff484848),
+                  fontSize: 34,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: 'Lato-Regular',
                 ),
-                Image(
-                  image: AssetImage('images/onboarding.png'),
-                ),
-                SizedBox(
-                  height: 93,
-                ),
-                Text(
-                  'Manage your finance in\nthe easiest way.',
-                  style: TextStyle(
-                    color: Color(0xff484848),
-                    fontSize: 34,
-                    fontWeight: FontWeight.w600,
-                    fontFamily: 'Lato-Regular',
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 48,
-                ),
-                SizedBox(
-                  height: 50,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                            Color(0xff025440),
-                          ),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ))),
-                      onPressed: (() {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (BuildContext) => SignUp()));
-                      }),
-                      child: Text(
-                        'Get Started',
-                        style: TextStyle(
-                          color: Color(0xffffffff),
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          fontFamily: 'Lato-Regular',
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 48,
+              ),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Color(0xff025440),
                         ),
-                      )),
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                RichText(
-                  text: TextSpan(
-                      text: 'Have an account?',
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ))),
+                    onPressed: (() {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (BuildContext) => SignUp()));
+                    }),
+                    child: Text(
+                      'Get Started',
                       style: TextStyle(
-                          color: Color(0xff484848),
-                          fontSize: 18,
-                          fontWeight: FontWeight.w700),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: ' Sign In ',
-                            style: TextStyle(
-                                color: Color(0xff025440),
-                                fontSize: 18,
-                                fontWeight: FontWeight.w700),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (BuildContext) => LogIn()));
-                              })
-                      ]),
-                ),
-              ],
-            ),
+                        color: Color(0xffffffff),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Lato-Regular',
+                      ),
+                    )),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              RichText(
+                text: TextSpan(
+                    text: 'Have an account?',
+                    style: TextStyle(
+                        color: Color(0xff484848),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: ' Sign In ',
+                          style: TextStyle(
+                              color: Color(0xff025440),
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (BuildContext) => LogIn()));
+                            })
+                    ]),
+              ),
+            ],
           ),
         ),
-      );
-    
+      ),
+    );
   }
 }
